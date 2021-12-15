33e35089253a0432a71214c700e8224fb598325f
Justin Yoo <justin.yoojh@gmail.com>
2021-12-15 21:15:37 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,18 +4,23 @@ set -e
 
 # Install oh-my-zsh
 echo "[$(date +"%Y-%m-%d %H:%M:%S")] Installing oh-my-zsh ..."
+echo "[$(date +"%Y-%m-%d %H:%M:%S")] While installing oh-my-zsh, you might be asked:"
+echo "[$(date +"%Y-%m-%d %H:%M:%S")]     'Do you want to change your default shell to zsh?'"
+echo "[$(date +"%Y-%m-%d %H:%M:%S")] Then type 'n' and continue ..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "[$(date +"%Y-%m-%d %H:%M:%S")] Installed oh-my-zsh. Type 'exit' to continue to install the remainders ..."
 
 # Install theme: Spaceship
 echo "[$(date +"%Y-%m-%d %H:%M:%S")] Installing theme: Spaceship ..."
-git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$HOME/.oh-my-zsh/custom/themes/spaceship-prompt" --depth=1
+ln -s "$HOME/.oh-my-zsh/custom/themes/spaceship-prompt/spaceship.zsh-theme" "$HOME/.oh-my-zsh/custom/themes/spaceship.zsh-theme"
 
 # Install plugins
 echo "[$(date +"%Y-%m-%d %H:%M:%S")] Installing plugins ..."
-git clone https://github.com/zsh-users/zsh-completions.git $ZSH_CUSTOM/plugins/zsh-completions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-git clone git://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-completions.git $HOME/.oh-my-zsh/custom/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone git://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
 # Copy .bashrc
 echo "[$(date +"%Y-%m-%d %H:%M:%S")] Replacing .bashrc ..."
